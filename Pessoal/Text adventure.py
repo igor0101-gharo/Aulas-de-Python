d82346ddf99e5981268f354397dc356ac9df9654
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-03 15:12:04 -0300

    aula

--- Pessoal/Text adventure.py
+++ Pessoal/Text adventure.py
@@ -19,30 +19,26 @@ def posicao (escolha):
         case "abrir o guarda roupa":
             print("\nVocê abriu o guarda roupa e encontrou uma chave antiga dentro dele.\n")
             escolha = input("Digite o que deseja fazer a seguir:\nabrir a porta\nabrir a janela\nabrir o guarda roupa\n")
-            posicao(escolha)
+            posicao2(escolha)
         case _:
             print("Opção inválida")
-            escolha = input("Digite o comando novamente")
-            posicao
+            escolha = input("Digite o comando novamente\n")
+            posicao(escolha)
 
 def posicao2 (escolha):
     match escolha:
         case "abrir a porta":
-            print("\nVocê tenta abrir a porta")
-            print("a porta está trancada\n")
-            escolha = input("Digite o que deseja fazer a seguir:\nabrir a porta\nabrir a janela\nabrir o guarda roupa\n")
-            posicao(escolha)
-
+            print("você abriu a porta e escapou.\nParabéns")
         case "abrir a janela":
             print("\nVocê se desequilibrou e caiu da janela. Você morreu.\n FIM")
         case "abrir o guarda roupa":
-            print("\nVocê abriu o guarda roupa e encontrou uma chave antiga dentro dele.\n")
+            print("\nNão há mais nada dentro do Guarda roupa.\n")
             escolha = input("Digite o que deseja fazer a seguir:\nabrir a porta\nabrir a janela\nabrir o guarda roupa\n")
-            posicao(escolha)
+            posicao2(escolha)
         case _:
             print("Opção inválida")
             escolha = input("Digite o comando novamente")
-            posicao
+            posicao2
 
 
 
