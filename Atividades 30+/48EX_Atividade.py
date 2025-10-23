7562f844c31a966ec1d1b2fd775dd39a69b229ae
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-23 16:43:13 -0300

    Aulas

---
+++ Atividades 30+/48EX_Atividade.py
@@ -0,0 +1,43 @@
+import os
+os.system("cls")
+from dataclasses import dataclass
+
+@dataclass
+class contato:
+    nome: str
+    email: str
+    endereco: str
+
+    def dados_entrega(self):
+        print(f"Nome: {self.nome}")
+        print(f"E-mail: {self.email}")
+        print(f"Endereço: {self.endereco}")
+
+    def dados_nome(self):
+        print(f"Nome: {self.nome}")
+
+lista_pessoas = []
+for i in range(2):
+    pessoa1 = contato(nome= input("Digite um nome:\n"),
+                    email=input("DIgite o um e-mail:\n"),
+                    endereco=input("Digite um endereço:\n"))
+    lista_pessoas.append(pessoa1)
+
+while True:
+    codigo = int(input("O que deseja?(digite o número da opção correspondente)\n1.Ver dados completos\n2.Ver apenas nome\n3.Sair"))
+    match codigo:
+        case 1:
+            for pessoa1 in lista_pessoas:
+                pessoa1.dados_entrega()
+            input("\n_")
+            os.system("cls")
+        case 2:
+            for pesso1 in lista_pessoas:
+                pessoa1.dados_nome()
+            input("\n_")
+            os.system("cls")
+        case 3:
+            print("Encerrando")
+            break
+        case _:
+            input("Opção inválida")
